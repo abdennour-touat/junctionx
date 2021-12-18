10f51e4dc540fdb742e21767ef0e685bb5f67c22
abdenour <touatabdenour33@gmail.com>
2021-12-18 07:24:03 +0100

    deploy commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from "./Components/Header";
 import PlasticArt from "./Components/PlasticArt";
 import Music from "./Components/Music";
 import Emergency from "./Components/Emergency";
+import NotFound from './Components/NotFound' 
 
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
         <Route path="PlasticArt" element={<PlasticArt />} />
         <Route path="Music" element={<Music/>}/>
         <Route path="Emergency" element={<Emergency />} />
+        <Route path="/*" element={<NotFound/>}/>
     </Routes>
   );
 }
---
+++ src/Components/NotFound.js
@@ -0,0 +1,15 @@
+import "./Header.css";
+
+
+const Header = () => {
+    console.log('hello')
+  return (
+    <div>
+      <div className="header">
+        <div className="logo"></div>
+        <h1 className=" text-center text-white text-5xl  pt-52 font-bold">404 error:Page not found!</h1> 
+      </div>
+    </div>
+  );
+};
+export default Header;
